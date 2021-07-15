90afe78afa8305cd39085c8849cdcef4f813fab9
B MacKenzie <53594331+DeltaWild@users.noreply.github.com>
2021-07-15 09:42:34 -0400

    Update FirebaseSpinnerAdapter.java

--- library/src/main/java/com/craft/libraries/firebaseuiaddon/FirebaseSpinnerAdapter.java
+++ library/src/main/java/com/craft/libraries/firebaseuiaddon/FirebaseSpinnerAdapter.java
@@ -1,6 +1,6 @@
 package com.craft.libraries.firebaseuiaddon;
 
-import android.support.annotation.LayoutRes;
+import androidx.annotation.LayoutRes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
